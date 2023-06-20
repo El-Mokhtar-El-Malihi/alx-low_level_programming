b307414710503f58f6ffd0d2c3af090cd6075d36
El-Mokhtar-El-Malihi <elmokhtarelmalihi@gmail.com>
2023-06-20 09:33:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+// main.h
 
-int main() {
-	    printf("_putchar\n");
-	        return 0;
-}
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int myFunction(); // Replace "myFunction" with the actual function name(s) you have in your program
+
+#endif /* MAIN_H */
+
