8cb67f097ebe3ffae606cb0a4eaa470f039e1463
El-Mokhtar-El-Malihi <elmokhtarelmalihi@gmail.com>
2023-06-22 12:56:48 -0700

    vi 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
-/**
- * _isupper - uppercase letters
- * @c: character to check
- *
- * Return: 0 or 1
- */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return 1;
-    else
-        return 0;
+    if (!isascii(c))
+        return 0; // Not a valid ASCII character
+
+    if (isupper(c))
+        return 1; // Uppercase character
+
+    return 0; // Not an uppercase character
 }
 
