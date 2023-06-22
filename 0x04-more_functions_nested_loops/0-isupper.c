537734aab062121571f4f60209ee683667c34544
El-Mokhtar-El-Malihi <elmokhtarelmalihi@gmail.com>
2023-06-22 13:00:36 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,15 +6,11 @@
  *
  * Return: 0 or 1
  */
-
 int _isupper(int c)
 {
-    if (!isascii(c))
-        return 0; 
-
-    if (isupper(c))
-        return 1; 
-
-    return 0; 
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    else
+        return 0;
 }
 
